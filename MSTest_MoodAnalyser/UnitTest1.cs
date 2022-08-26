6dfde658f289e99161dca6817afbb0849daa394a
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-27 02:10:55 +0530

    [Hemant] Passing SAD Message in constructor should return SAD

---
+++ MSTest_MoodAnalyser/UnitTest1.cs
@@ -0,0 +1,31 @@
+using MoodAnalyserProb;
+
+namespace MSTest_MoodAnalyser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Test Case 1.1  Given I am in Sad Mood Should return Sad Mood
+
+        [TestMethod]
+        public void GivenSADMood_ShouldReturnSAD()
+        {
+
+            //Arrange
+
+            string message = "I am in Sad Mood";
+            MoodAnalyser obj = new MoodAnalyser(message);
+
+            string expected = "Sad Mood";
+
+            //Act
+
+            string actual=obj.AnalyseMood();
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+      
+    }
+}
