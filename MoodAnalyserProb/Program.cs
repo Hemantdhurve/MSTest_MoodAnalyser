9601d335aa64c9a18bfa92553af48a19e617dab7
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-27 00:04:50 +0530

    Welcome Message

---
+++ MoodAnalyserProb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserProb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+        }
+    }
+}
