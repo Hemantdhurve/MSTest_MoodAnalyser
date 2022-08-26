747c20c50ddb15f428139eb83c421ba89c68ca22
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-27 01:14:05 +0530

    [Hemant] Test Case 1.1 Passing Sad should return Sad Mood

---
+++ MSTest_MoodAnalyser/UnitTest1.cs
@@ -0,0 +1,31 @@
+using MoodAnalyserProb;
+
+namespace MSTest_MoodAnalyser
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Test Case 1.1  Given I am in Sad Mood Should return Sad Mood
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange
+
+            MoodAnalyser obj = new MoodAnalyser();
+            
+            string message = "I am in Sad Mood";                 //we can also pass this message directly in the object
+
+            string expected = "Sad Mood";
+
+            //Act
+
+            string actual=obj.AnalyseMood(message);
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
