13f22ec237dd26c67c5cc960c44dd3eb4428b40f
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-27 02:16:16 +0530

    [Hemant] Passing Happy Message in Constructor Should return Happy

--- MSTest_MoodAnalyser/UnitTest1.cs
+++ MSTest_MoodAnalyser/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace MSTest_MoodAnalyser
     [TestClass]
     public class UnitTest1
     {
-        //Test Case 1.1  Given I am in Sad Mood Should return Sad Mood
+        //Test Case 1.1  Given I am in Sad Mood Message Should return Sad Mood
 
         [TestMethod]
         public void GivenSADMood_ShouldReturnSAD()
@@ -26,6 +26,28 @@ namespace MSTest_MoodAnalyser
 
             Assert.AreEqual(expected, actual);
         }
-      
+
+
+        //Test Case 1.2  Given I am in Happy Mood Message Should return Happy Mood
+
+        [TestMethod]
+        public void GivenHappyMoodMessage_ShouldReturnHAPPY()
+        {
+
+            //Arrange
+
+            string message = "I am in Happy Mood";
+            MoodAnalyser obj = new MoodAnalyser(message);
+
+            string expected = "Happy Mood";
+
+            //Act
+
+            string actual = obj.AnalyseMood();
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
