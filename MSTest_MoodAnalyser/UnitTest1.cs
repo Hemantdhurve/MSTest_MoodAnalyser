25101a3cbe0f0f1e9767e5c2a3132b0ca064b0ca
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-27 01:19:36 +0530

    [Hemant] Test Case 1.1 Passing Any mood should return Happy

--- MSTest_MoodAnalyser/UnitTest1.cs
+++ MSTest_MoodAnalyser/UnitTest1.cs
@@ -27,5 +27,29 @@ namespace MSTest_MoodAnalyser
 
             Assert.AreEqual(expected, actual);
         }
+
+
+        //Test Case 1.2  Given I am in Any Mood Should return Happy Mood
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
+            //Arrange
+
+            MoodAnalyser obj = new MoodAnalyser();
+
+            string message = "I am in Any Mood";                 //we can also pass this message directly in the object
+
+            string expected = "Happy Mood";
+
+            //Act
+
+            string actual = obj.AnalyseMood(message);
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
