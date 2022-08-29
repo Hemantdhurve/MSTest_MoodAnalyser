c56d18bfa9c24fd6a1689a3b88ea59db90c9b396
Hemantdhurve <hemantdhurve007@gmail.com>
2022-08-29 16:30:34 +0530

    [Hemant] Creating MoodAnalyser with Parameter Constructor

--- MoodAnalyserProb/MoodAnalyserFactory.cs
+++ MoodAnalyserProb/MoodAnalyserFactory.cs
@@ -43,6 +43,30 @@ namespace MoodAnalyserProb
                 return e.Message;
             }
         }
-        
+
+        //UC5
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not Found");
+
+            }
+        }
+
     }
 }
